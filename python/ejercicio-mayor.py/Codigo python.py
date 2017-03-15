05497e9a985263ce084f1b8df2fe00f35c3c9c2a
Reyxod <reyxod@gmail.com>
2017-03-15 22:02:24 +0100

    Update Codigo python.py

--- python/ejercicio-mayor.py/Codigo python.py
+++ python/ejercicio-mayor.py/Codigo python.py
@@ -12,30 +12,25 @@ while entrar == True :
 		os.system('clear')
 		numero1 = float(raw_input("Introduce el primer número: "))
 		numero2 = float(raw_input("Introduce el segundo número: "))
-		
+		os.system('clear')
 	
 		if numero1 == numero2 :
-			
-			os.system('clear')
+
 			print "No puedes poner dos números iguales\n"
 			tecla = raw_input("Pulsa enter para volver a intentar")	
 			
 		if numero1 > numero2 :
 			
-			os.system('clear')
 			print "El número mas grande es :",numero1
 			entrar = False
 		
 		if numero2 > numero1 :
 			
-			os.system('clear')
 			print "El número mas grande es :",numero2
 			entrar = False
-			
-		
+
 	except ValueError :
 		
 		os.system('clear')
 		print "Eso no es un número\n"
-		tecla = raw_input("Pulsa enter para volver a intentar")	
-		os.system('clear')
+		tecla = raw_input("Pulsa enter para volver a intentar")
