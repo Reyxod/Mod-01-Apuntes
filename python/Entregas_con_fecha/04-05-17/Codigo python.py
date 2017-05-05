67a9ed1259ea8ef4ef8ec7776937a853bb143f3d
Reyxod <reyxod@gmail.com>
2017-05-05 08:50:58 +0200

    Create Codigo python.py

---
+++ python/Entregas_con_fecha/04-05-17/Codigo python.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+cont = 1
+salir = False
+
+while salir == False :
+	
+  print cont
+	
+  if cont ==3 :
+		
+    salir=True
+
+  contador= contador +1
