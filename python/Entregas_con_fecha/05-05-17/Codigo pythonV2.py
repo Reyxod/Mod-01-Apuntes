23d9b8dc09dad75354b99713743f6da0cf8b3f7b
Reyxod <reyxod@gmail.com>
2017-05-05 23:53:26 +0200

    Create Codigo pythonV2.py

---
+++ python/Entregas_con_fecha/05-05-17/Codigo pythonV2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def mi_rango(inicio,final,incremento):
+	
+	if inicio <= final :
+  
+		while inicio <= final :
+    
+			yield inicio
+			inicio = inicio + incremento
+      
+	else:
+  
+		while inicio >= final :
+    
+			yield inicio
+			inicio = inicio - incremento
+
+for fila in mi_rango(1,5,1):
+
+	print ""
+  
+	for columna in mi_rango(1,4,1):
+  
+		if fila == 1:		
+    
+			if columna == 2:
+      
+				print "A",
+        
+			elif columna == 3:
+      
+				print "B",
+        
+			elif columna == 4:
+      
+				print "C",
+        
+			else:
+      
+				print "-",
+	
+		elif columna == 1:
+    
+			print fila-1,
+      
+		else:
+      
+      if (( columna == 2 and fila == 4 ) or ( columna == 3 and fila == 3 ) :
+        
+        print "*"
+      
+      else :
+      
+		  	print "-",
